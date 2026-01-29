97a17e8b625833fb1aca9e54bc2853340f4866d7
asaf-ahmed <asaf1582003@gmail.com>
2026-01-29 10:08:47 +0530

    first commit

---
+++ day_01/promises/index.js
@@ -0,0 +1,26 @@
+const mypromise = new Promise((resolve, reject) => {
+    let age = 22;
+    if (age >= 18) {
+        resolve("You are eligible to vote.");
+    } else {
+        reject("You are not eligible to vote.");
+    }
+});
+
+// console.log(mypromise);
+
+// mypromise.then((message) => {
+//     console.log("Resolved: " + message);
+// }).catch((error) => {
+//     console.log("error occured: " + error);
+// });
+
+const f1 = async () => {
+    const msg = await mypromise;
+    console.log(msg);
+}
+
+
+
+
+
