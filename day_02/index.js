f380eabdad8ffbb80d57fc2b650e1f5d122df9c8
asaf-ahmed <asaf1582003@gmail.com>
2026-02-05 09:47:38 +0530

    write, read -function

--- day_02/index.js
+++ day_02/index.js
@@ -1,7 +1,21 @@
 import fs from 'fs';
 
 const writeFileSync =(path, data)=>{
+    try{
     fs.writeFileSync(path,data);
     console.log("data has written successfully");
+}catch(error){
+    console.log("error occured: ");
+}
 }
 writeFileSync("./example.txt","this data is written through sync fun:'writerfilesync())'");
+
+const readFileSync =(path)=>{
+    try{
+    const data = fs.readFileSync(path,"utf-8");
+    console.log(data);
+}catch(error){
+    console.log("unable to read data");
+}   
+}
+readFileSync("./example.txt");
